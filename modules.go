5c1ac689df8551ec9edd1bfece5000026810df06
Leon Gibat <brendan.gibat@gmail.com>
2016-03-30 11:13:33 -0400

    updating formatting

--- modules.go
+++ modules.go
@@ -1,7 +1,7 @@
 package main
 
 import (
+	_ "github.com/brendangibat/logspout-redis-logstash"
 	_ "github.com/gliderlabs/logspout/httpstream"
 	_ "github.com/gliderlabs/logspout/routesapi"
-	_ "github.com/brendangibat/logspout-redis-logstash"
 )
